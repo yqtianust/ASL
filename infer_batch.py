b334b2b80b919e6f04b8452c207f5993b459e8f0
ytianas <ytianas@yqgpu1>
2020-10-02 19:42:33 +0800

    update infer_batch

--- infer_batch.py
+++ infer_batch.py
@@ -63,7 +63,7 @@ def load_model(model_type):
     return model, input_size, threshold, num_classes, classes_list
 
 
-def test():
+def test_single():
 
     model, input_size, threshold, num_classes, classes_list = load_model("L")
 
@@ -83,6 +83,90 @@ def test():
     print(labels)
     print(labels_probs)
 
+
+def test_with_loader():
+
+    model, input_size, threshold, num_classes, classes_list = load_model("L")
+
+
+    from torchvision.transforms import transforms
+    from data_loader import CocoObject
+    from torch.autograd import Variable
+    from sklearn.metrics import average_precision_score
+    import torch.nn as nn
+    from tqdm import tqdm as tqdm
+    # crop_size = 224
+    # image_size = 256
+    batch_size = 12
+    # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+    #                                  std=[0.229, 0.224, 0.225])
+
+    val_transform = transforms.Compose([
+        transforms.Resize([input_size, input_size]),
+        # transforms.CenterCrop(crop_size),
+        transforms.ToTensor(),
+        ])
+
+    # Data samplers.
+    ann_dir = '/home/ytianas/EMSE_COCO/cocodataset/annotations'
+    image_dir = '/home/ytianas/EMSE_COCO/cocodataset/'
+    test_data = CocoObject(ann_dir=ann_dir, image_dir=image_dir,
+                           split='test', transform=val_transform)
+    image_ids = test_data.image_ids
+    image_path_map = test_data.image_path_map
+    # 80 objects
+    id2object = test_data.id2object
+    id2labels = test_data.id2labels
+    # Data loaders / batch assemblers.
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size,
+                                              shuffle=False, num_workers=4,
+                                              pin_memory=True)
+    count = 0
+    yhats = []
+    labels = []
+    imagefiles = []
+    res = list()
+
+    t = tqdm(test_loader, desc = 'testing')
+
+    for batch_idx, (images, objects, image_ids) in enumerate(t):
+
+        images = Variable(images).cuda()
+        objects = Variable(objects).cuda()
+
+        # print(images.shape)
+
+        object_preds = model(images)
+        m = nn.Sigmoid()
+        object_preds_r = m(object_preds)
+
+        count = count + len(image_ids)
+        for i in range(len(image_ids)):
+            image_file_name = image_path_map[int(image_ids[i])]
+            yhat = []
+            label = id2labels[int(image_ids[i])]
+
+            for j in range(len(object_preds[i])):
+                a = object_preds_r[i][j].cpu().data.numpy()
+                if a > threshold:
+                    yhat.append(id2object[j])
+
+            yhats.append(yhat)
+            labels.append(label)
+            imagefiles.append(image_file_name)
+
+
+        res.append((image_ids, object_preds.data.cpu(), objects.data.cpu()))
+        if count % 1000 == 0:
+            print("count: " + str(count))
+
+    preds_object = torch.cat([entry[1] for entry in res], 0)
+    targets_object = torch.cat([entry[2] for entry in res], 0)
+    eval_score_object = average_precision_score(targets_object.numpy(), preds_object.numpy())
+    print('\nmean average precision of object classifier on test data is {}\n'.format(eval_score_object))
+
+
 if __name__ == '__main__':
 
-    test()
+    # test_single()
+    test_with_loader()
